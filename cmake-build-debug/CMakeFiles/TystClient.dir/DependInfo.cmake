1665510e0481cf9816f99ad1629ba92b6159d57d
Herman <Herminatorb@gmail.com>
2020-09-08 13:26:05 +0200

    Template code

--- cmake-build-debug/CMakeFiles/TystClient.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/TystClient.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/Users/john.blaberg/Desktop/TystChat/TystClient/main.c" "/Users/john.blaberg/Desktop/TystChat/TystClient/cmake-build-debug/CMakeFiles/TystClient.dir/main.c.o"
+  "/Users/Herman/Desktop/Projects/TystClient/main.c" "/Users/Herman/Desktop/Projects/TystClient/cmake-build-debug/CMakeFiles/TystClient.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "AppleClang")
 
