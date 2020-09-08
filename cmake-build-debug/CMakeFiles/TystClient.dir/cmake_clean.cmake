7c2a3e1abbb43fd54f83e76cf4c1f04da5a8540b
John Blåber Kristoffersson <John.kristoffersson@gmail.com>
2020-09-08 11:12:44 +0200

    created the initial project for Client

---
+++ cmake-build-debug/CMakeFiles/TystClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TystClient.dir/main.c.o"
+  "TystClient"
+  "TystClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TystClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
